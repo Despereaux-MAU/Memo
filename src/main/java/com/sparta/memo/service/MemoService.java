1f5a89c51d00d4c9937450e9ab685754a029239b
despereaux <157133321+Despereaux-MAU@users.noreply.github.com>
2024-10-03 16:43:08 +0900

    IoC Container와 Bean

--- src/main/java/com/sparta/memo/service/MemoService.java
+++ src/main/java/com/sparta/memo/service/MemoService.java
@@ -4,10 +4,14 @@ import com.sparta.memo.dto.MemoRequestDto;
 import com.sparta.memo.dto.MemoResponseDto;
 import com.sparta.memo.entity.Memo;
 import com.sparta.memo.repsitory.MemoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 
-public class MemoService {
+@Service
+public class MemoService { // memoService
 
     private final MemoRepository memoRepository;
 
