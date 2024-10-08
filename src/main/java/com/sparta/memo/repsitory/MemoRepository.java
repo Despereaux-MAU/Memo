6ed0fd5832f21b2d1fa586cd12f0ed670c99c0a5
Despereaux <atousa0507@gmail.com>
2024-10-08 12:40:11 +0900

    Query Methods란 무엇일까?

--- src/main/java/com/sparta/memo/repsitory/MemoRepository.java
+++ src/main/java/com/sparta/memo/repsitory/MemoRepository.java
@@ -3,5 +3,8 @@ package com.sparta.memo.repsitory;
 import com.sparta.memo.entity.Memo;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface MemoRepository extends JpaRepository<Memo,Long> { // memoRepository
+    List<Memo> findAllByOrderByModifiedAtDesc();
 }
